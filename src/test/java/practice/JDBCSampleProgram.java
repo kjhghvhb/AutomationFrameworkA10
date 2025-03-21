a6d79e7309ed5c0982d5d4f3342c02a60ef33533
hp <hp@DESKTOP-GURU-NATIKAR>
2025-03-21 16:39:52 +0530

    jdbc connection committed

---
+++ src/test/java/practice/JDBCSampleProgram.java
@@ -0,0 +1,44 @@
+package practice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.testng.annotations.Test;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class JDBCSampleProgram
+{
+	@Test()
+	public void  SelectQuerySample() throws SQLException
+	{
+		//fetch the driver from database
+		Driver dref=new Driver();
+		
+		//Register the driver 
+		DriverManager.registerDriver(dref);
+		
+		//get the connection with driver=give database name
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/customerdb", "root", "root");
+		
+		//issues create Statement 	
+	Statement state = con.createStatement();
+	
+	//execute the query -> table name
+	ResultSet res = state.executeQuery("select * from customerinfo");
+	
+	while(res.next())
+	{
+		System.out.println(res.getString(1) +"_" +res.getInt(2) +"_" + res.getString(3));
+	}
+	
+	//close the database
+	con.close();
+	}
+	
+	
+
+}
